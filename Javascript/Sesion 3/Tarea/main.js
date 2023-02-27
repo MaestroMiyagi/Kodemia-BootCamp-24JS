2c78eb6418066926d34d8a33e4d747bbd974c3fc
MaestroMiyagi <mffchmono@gmail.com>
2023-02-27 16:56:08 -0600

    feat: added exercise 2 and 3

--- Javascript/Sesion 3/Tarea/main.js
+++ Javascript/Sesion 3/Tarea/main.js
@@ -5,16 +5,29 @@ function palindrome(phrase) {
     phrase = phrase.replace(/[^a-zA-Z]/g, "").toLowerCase();
     console.log(phrase.replace(/[^a-zA-Z]/g, "").toLowerCase())
     return phrase === phrase.split("").reverse().join("");
-} 
+}
 console.log(`${palindrome(phrase)}`)
 
 phrase = 'A la grande le puse cuca'
 console.log(`${palindrome(phrase)}`)
 
 /* Ejercicio 2 */
+function capitalLetters(phrase) {
+    return phrase.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+console.log(`${capitalLetters(phrase)}`)
 
-/* 2.-Crear una función que recibe un string con varias palabras y cambia a mayúscula la primer letra de cada palabra
-21:50
-3.- Crear una función que recibe un string con multiples palabras y regresa únicamente la palabra mas larga en la oración
-21:51
-4.-Crear una función que recibe 2 argumentos una cadena de texto y un carácter, la función regresa un número que representa el número de veces que se repite el carácter en la cadena dada */
+/* Ejercicio 3 */
+function longest(phrase) {
+    const words = phrase.split(' ');
+    let longboi = '';
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longboi.length) {
+            longboi = words[i];
+        }
+    }
+    return longboi;
+}
+console.log(`${longest(phrase)}`)
+
+/* Ejercicio 4 */
