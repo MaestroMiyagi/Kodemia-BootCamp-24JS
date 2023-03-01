f059effb1d3a87755c6c32f7befb7371c8621497
MaestroMiyagi <mffchmono@gmail.com>
2023-03-01 12:47:25 -0600

    feat: finished class exercises

--- Javascript/Sesion 5/main.js
+++ Javascript/Sesion 5/main.js
@@ -1,18 +1,38 @@
-let carsArray =[
-    {
-      name: "Toledo",
-      branch: "Seat",
-      year: "2017",
-      hp: 125,
-      price: 300000,
-      presentation: () => {
-        console.log("Seat Toledo por tan sólo $300,000 MXN!!!");
+let carsArray = [
+  {
+    name: "Toledo",
+    branch: "Seat",
+    year: "2017",
+    hp: 125,
+    price: 300000,
+    presentation: () => {
+      console.log("Seat Toledo por tan sólo $300,000 MXN!!!");
+    },
+  },
+  {
+    name: "Chevy",
+    branch: "Chevrolet",
+    year: "2010",
+    hp: 125,
+    price: 90000,
+    presentation: () => {
+      console.log("Super chevy!!!")
       },
-    }
-  ] ;
-
-  carsArray.forEach((car) => car.presentation());
-
-  const getMonthlyPayments = (months)  => {
-    carsArray.forEach((car) => console.log(``)
   }
+];
+
+carsArray.forEach((car) => car.presentation());
+
+const getMonthlyPayments = (months) => {
+  carsArray.forEach((car) =>
+    console.log(`Los pagos mensuales del ${car.branch} ${car.name} serían de de ${car.price / months}`)
+  );
+}; 
+
+/* const getMonthlyPayments = (months) => {
+  carsArray.forEach((item, index, array) => {
+    calculateMonthlyPayment(item, months)
+  })
+} */
+
+getMonthlyPayments(6)
