685f20607722993ef58599f08578063686425bdd
MaestroMiyagi <mffchmono@gmail.com>
2023-03-01 12:34:44 -0600

    feat: added exercises

---
+++ Javascript/Sesion 5/main.js
@@ -0,0 +1,18 @@
+let carsArray =[
+    {
+      name: "Toledo",
+      branch: "Seat",
+      year: "2017",
+      hp: 125,
+      price: 300000,
+      presentation: () => {
+        console.log("Seat Toledo por tan sólo $300,000 MXN!!!");
+      },
+    }
+  ] ;
+
+  carsArray.forEach((car) => car.presentation());
+
+  const getMonthlyPayments = (months)  => {
+    carsArray.forEach((car) => console.log(``)
+  }
