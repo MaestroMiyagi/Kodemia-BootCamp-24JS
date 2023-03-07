83d480a9aa8e8312e16552999dbc6ff1f3d12ff6
Iyari Maldonado <mffchmono@gmail.com>
2023-03-06 22:27:15 -0600

    feat: added class exercise attempt

---
+++ Javascript/Sesion 8/main.js
@@ -0,0 +1,23 @@
+ages=[{
+    name:"Isra",
+    age:09/27/1989
+  }
+  ,{
+    name:"Rodolfo",
+    age:11/27/1998
+  }
+  ,{
+    name:"Kim",
+    age:11/15/1996
+  }
+  ,{
+    name:"Iyari",
+    age:09/26/1997
+  }
+  ]
+  
+  function differenceInAge(ages){
+    return [Math.min(...ages.age), Math.max(...ages.age), Math.max(...ages.age)-Math.min(...ages.age)];
+  }
+  
+  console.log(differenceInAge(ages))
