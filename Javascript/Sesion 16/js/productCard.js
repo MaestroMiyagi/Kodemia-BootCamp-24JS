b55c78911d6ef9d5703ec08ee2a599f7fc36ffc5
MaestroMiyagi <mffchmono@gmail.com>
2023-03-18 12:59:47 -0600

    feat: fixed card grid

--- Javascript/Sesion 16/js/productCard.js
+++ Javascript/Sesion 16/js/productCard.js
@@ -1,6 +1,6 @@
 const productCard = (productName, productPrice, productImage, productDescription, productCategory, productId, buttonFunction) => {
     let principalDiv = document.createElement("div")
-    principalDiv.classList.add("card","col-md-3", "mt-3")
+    principalDiv.classList.add("card","col-3", "mt-3")
 
 
     let principalImage = document.createElement("img")
